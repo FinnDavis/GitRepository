e4af0ae862cc805da55a13502e7895d3a2cc6419
AIE\s213398 <s213398@students.aie.edu.au>
2022-05-26 16:42:38 +1000

    fully complete game with comments

--- Pong/RaylibStarterCPP/Game.cpp
+++ Pong/RaylibStarterCPP/Game.cpp
@@ -10,46 +10,48 @@ void::GameWindow::Init()
 	SetTargetFPS(targetFps);
 	rng.seed(time(nullptr));
 	range = std::uniform_real_distribution<float>(-PI / 4, PI / 4);
+	icon = LoadImage("../PongIcon.PNG");
+	SetWindowIcon(icon);
 }
 
 void::GameWindow::Update()
 {
+	// Test if game has started or game over
 	if (gameStart && !gameOver)
-	{
+	{	
+		// Player 1 controls (Left paddle)
 		if (IsKeyDown(KEY_W) && player1Pos.y > 0 && !IsKeyDown(KEY_DOWN))
 		{
-			player1Pos = Vector2Add(player1Pos, Vector2Scale(Vector2{ 0, -225 }, GetFrameTime()));
+			player1Pos = Vector2Add(player1Pos, Vector2Scale(Vector2{ 0, -(float)screenHeight}, GetFrameTime()));
 		}
 		else if (IsKeyDown(KEY_S) && player1Pos.y < screenHeight - playerHight && !IsKeyDown(KEY_UP))
 		{
-			player1Pos = Vector2Add(player1Pos, Vector2Scale(Vector2{ 0, 225 }, GetFrameTime()));
+			player1Pos = Vector2Add(player1Pos, Vector2Scale(Vector2{ 0, (float)screenHeight }, GetFrameTime()));
 		}
 
+		// Player 2 controls (Right paddle)
 		if (IsKeyDown(KEY_UP) && player2Pos.y > 0 && !IsKeyDown(KEY_DOWN))
 		{
-			player2Pos = Vector2Add(player2Pos, Vector2Scale(Vector2{ 0, -225 }, GetFrameTime()));
+			player2Pos = Vector2Add(player2Pos, Vector2Scale(Vector2{ 0, -(float)screenHeight }, GetFrameTime()));
 		}
 		else if (IsKeyDown(KEY_DOWN) && player2Pos.y < screenHeight - playerHight && !IsKeyDown(KEY_UP))
 		{
-			player2Pos = Vector2Add(player2Pos, Vector2Scale(Vector2{ 0, 225 }, GetFrameTime()));
-		}
-		if (IsKeyReleased(KEY_Z))
-		{
-			if (debug)
-			{
-				debug = false;
-			}
-			else
-			{
-				debug = true;
-			}
+			player2Pos = Vector2Add(player2Pos, Vector2Scale(Vector2{ 0, (float)screenHeight }, GetFrameTime()));
 		}
 
+		// Toggle debug mode
+		if (IsKeyReleased(KEY_Z))
+			if (debug) debug = false;
+			else debug = true;
+		
+		// Update ball position
 		ballPos = Vector2Add(ballPos, Vector2Scale(ballVelocity, GetFrameTime()));
 
+		// Point used to decid the distance from the middle of the paddle to where the ball hit the paddle
 		player1CurvePoint = Vector2{ player1Pos.x - 80, player1Pos.y + playerHight / 2 };
 		player2CurvePoint = Vector2{ player2Pos.x + 100, player2Pos.y + playerHight / 2 };
 
+		// Update AABB's
 		player1AABB.min = Vector3{ player1Pos.x, player1Pos.y, 1 };
 		player1AABB.max = Vector3{ player1Pos.x + playerWidth, player1Pos.y + playerHight, 1 };
 
@@ -59,41 +61,47 @@ void::GameWindow::Update()
 		ballAABB.min = Vector3{ ballPos.x - ballRadius, ballPos.y - ballRadius, 1 };
 		ballAABB.max = Vector3{ ballPos.x + ballRadius, ballPos.y + ballRadius, 1 };
 
+		// Test Top and Bottom border collision
 		if (edgeTop.TestSide(Vector3{ ballPos.x, ballPos.y, 0 }, ballRadius) == Plane::ePlaneResult::INTERSECTS && ballVelocity.y < 0)
 			ballVelocity = Vector2{ ballVelocity.x, -ballVelocity.y };
 		if (edgeBottom.TestSide(Vector3{ ballPos.x, ballPos.y, 0 }, ballRadius) == Plane::ePlaneResult::INTERSECTS && ballVelocity.y > 0)
 			ballVelocity = Vector2{ ballVelocity.x, -ballVelocity.y };
 
+		// Test Left and Right border collision
 		if (edgeLeft.TestSide(Vector3{ ballPos.x, ballPos.y, 0 }, ballRadius) == Plane::ePlaneResult::INTERSECTS
 		 || edgeRight.TestSide(Vector3{ ballPos.x, ballPos.y, 0 }, ballRadius) == Plane::ePlaneResult::INTERSECTS)
 			gameOver = true;
 
+		// Test paddle collision
+		//
+		// V = Velocity vector
+		// N = Surface normal	
+		// V-2N(V.N)
 		if (ballAABB.Overlaps(player1AABB) && ballVelocity.x < 0)
 		{
-			score += 1;
 			Vector2 normal = Vector2Normalize(Vector2Subtract(ballPos, player1CurvePoint));
-			// V = Velocity vector
-			// N = Surface normal
-			// 
-			// V-2N(V.N)
 			ballVelocity = Vector2Subtract(ballVelocity, Vector2Scale(Vector2Scale(normal, 2), Vector2DotProduct(ballVelocity, normal)));
+			ballVelocity = Vector2Scale(ballVelocity, 1.05);
 			ballPos = Vector2Add(ballPos, Vector2Scale(ballVelocity, GetFrameTime()));
 		}
 		if (ballAABB.Overlaps(player2AABB) && ballVelocity.x > 0)
 		{
 			Vector2 normal = Vector2Normalize(Vector2Subtract(ballPos, player2CurvePoint));
 			ballVelocity = Vector2Subtract(ballVelocity, Vector2Scale(Vector2Scale(normal, 2), Vector2DotProduct(ballVelocity, normal)));
+			ballVelocity = Vector2Scale(ballVelocity, 1.05);
 			ballPos = Vector2Add(ballPos, Vector2Scale(ballVelocity, GetFrameTime()));
 		}
 	}
 	else
 	{
+		// Wait for user input to start/restart the game
 		if (IsKeyReleased(KEY_SPACE))
 		{
 			gameOver = false;
 			gameStart = true;
 			float startingAngle = range(rng);
-			player1Pos = Vector2{ 20, 10 };
+			player1Pos = Vector2{ 20, (float)screenHeight / 2 - (float)playerHight / 2};
+			player2Pos = Vector2{ (float)screenWidth - playerWidth - 20, (float)screenHeight / 2 - (float)playerHight / 2 };
 			ballPos = Vector2{ (float)(screenWidth - 5) / 2, (float)(screenHeight - 5) / 2 };
 			ballVelocity = Vector2Scale(Vector2{ cos(startingAngle), sin(startingAngle) }, ballSpeed);
 		}
@@ -108,9 +116,7 @@ void::GameWindow::Draw()
 
 		DrawRectangle((int)player1Pos.x, (int)player1Pos.y, playerWidth, playerHight, WHITE);
 		DrawRectangle((int)player2Pos.x, (int)player2Pos.y, playerWidth, playerHight, WHITE);
-		DrawCircle((int)ballPos.x, (int)ballPos.y, ballRadius, WHITE);
-
-	
+ 		DrawCircle((int)ballPos.x, (int)ballPos.y, ballRadius, WHITE);
 
 		if (debug)
 			DrawDebug();
--- Pong/RaylibStarterCPP/Game.h
+++ Pong/RaylibStarterCPP/Game.h
@@ -12,49 +12,59 @@ namespace Game
 	class GameWindow
 	{
 	private:
+
+		// Image
+		Image icon = Image{};
+
+		// Bool
 		bool gameStart = false;
 		bool gameOver = false;
 		bool debug = false;
 		
-		int score = 0;
+		// Int
+		int screenHeight = 450;
+		int screenWidth = 800;
 
 		int playerWidth = 20;
 		int playerHight = 80;
+		int ballSpeed = 300;
+		int targetFps = 60;
+		int fontSize = 20;
+
+		// Float
 		float ballRadius = 10;
 
-		int ballSpeed = 300;
-
-		int screenWidth = 800;
-		int screenHeight = 450;
-		int targetFps = 60;
-
-		std::mt19937 rng;
+		// Rng
 		std::uniform_real_distribution<float> range;
+		std::mt19937 rng;
 
+		// Plane
 		Plane edgeTop = { 0, 1, 0 };
 		Plane edgeBottom = { 0, -1, (float)screenHeight };
 		Plane edgeLeft = { 1, 0, 0 };
 		Plane edgeRight = { -1, 0, (float)screenWidth };
 
-		Vector2 player1CurvePoint;
-		Vector2 player2CurvePoint;
-
+		// Vector 2
 		Vector2 player1Pos = Vector2{ 20, 10 }; 
 		Vector2 player2Pos = Vector2{ (float)screenWidth - 40, 10 };
 		Vector2 ballPos = Vector2{ (float)(screenWidth - 5) / 2, (float)(screenHeight - 5) / 2 };
 		Vector2 ballVelocity = Vector2{ 0, 0 };
+		Vector2 player1CurvePoint;
+		Vector2 player2CurvePoint;
 
+		// AABB (Axis Aligned Bounding Box)
 		AABB player1AABB;
 		AABB player2AABB;
 		AABB ballAABB;
 
-		const char* title = "Pong";
-		const char* startText = "Press space to start!";
+		// const char (strings)
 		const char* gameOverText = "        Game Over!\nPress space to restart.";
-		int fontSize = 20;
-
+		const char* startText = "Press space to start!";
+		const char* title = "Pong";
 
 	public:
+
+		// Functions
 		void Init();
 		void Update();
 		void Draw();
